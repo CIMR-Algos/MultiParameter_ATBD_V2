d512aeec9bcd4e1c39dcfcee5bf3546d93f23f1b
Marcus Huntemann <huntemann@iup.physik.uni-bremen.de>
2024-09-27 19:39:35 +0200

    mv and upd mpr_run rutine

--- algorithm/mpr_run.jl
+++ algorithm/mpr_run.jl
@@ -11,39 +11,47 @@
 #julia run_mp.jl l1b_file.nc output_file.nc 
 
 ##
-include("OEM_with_SIT.jl")
-using .OEM
+using Revise
+includet(joinpath(@__DIR__(),"../book/common_functions.jl"))
+#using OEM
 
-
+inpath = get(ENV, "DEVALGO_INPUT_DATA_PATH", "/mnt/spaces/Projects/2022_CIMR-DEVALGO/DATA")
 if isempty(ARGS)
-   fn = "/mnt/spaces/Projects/2022_CIMR-DEVALGO/SCEPS_l1b_sceps_geo_polar_scene_1_unfiltered_tot_minimal_nom_nedt_apc_tot_v2p1.nc"
-   outfn = "out_polar.nc"
+    fn=joinpath(inpath,"SCEPS/SCEPS_l1b_sceps_geo_polar_scene_1_unfiltered_tot_minimal_nom_nedt_apc_tot_v2p1.nc")
 else
     fn = ARGS[1]
+    @assert isfile(fn)
+end
+
+if length(ARGS) < 2
+   outfn = "out_polar.nc"
+else
     outfn = ARGS[2]
 end
 
-outgrid = length(ARGS) >= 3 ? ARGS[3] : "ease2_nh"
-direction = "forward"
+if length(ARGS) < 3
+    outgrid = "ease2_nh"
+else
+    outgrid = ARGS[3]
+    @assert outgrid in ["ease2_nh", "ease2_sh"]
+end
+
 
 #forward
 findata,finerr,flat,flon=project_data(fn,"C_BAND","forward")
-println(sizeof(findata))
-println(sizeof(finerr))
 
-fdat,ferr,_=run_oem(findata,finerr)
+fdat,ferr,fres,fit=run_oem(findata,band_error=finerr)
 #backward
 bindata,binerr,blat,blon=project_data(fn,"C_BAND","backward")
-bdat,berr,_=run_oem(bindata,binerr)
+bdat,berr,bres,bit=run_oem(bindata,band_error=binerr)
 ##
 
-thedata=(;forward=(;data=fdat,err=ferr,lat=flat,lon=flon),
-         backward=(;data=bdat,err=berr,lat=blat,lon=blon))
+thedata=(;forward=(;data=fdat,err=ferr,lat=flat,lon=flon,res=fres,it=fit),
+         backward=(;data=bdat,err=berr,lat=blat,lon=blon,res=bres,it=bit))
 
 
 println("storing output ", outfn, ", for grid ", outgrid)
 store_output(outfn, thedata, outgrid)
-
 
 
 
